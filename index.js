fb123d1a21f84cc6a77bf2f74d4ca5b97870b8ee
dev-rohit0 <rohit.codein@gmail.com>
2023-02-23 11:46:45 +0530

    code formatted

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const listItems = document.querySelectorAll("#list li");
-		const target = document.querySelector("#target");
+const target = document.querySelector("#target");
 
-		for (const listItem of listItems) {
-		  listItem.addEventListener("dragstart", dragStart);
-		}
+for (const listItem of listItems) {
+	listItem.addEventListener("dragstart", dragStart);
+}
 
-		target.addEventListener("dragover", dragOver);
-		target.addEventListener("drop", drop);
+target.addEventListener("dragover", dragOver);
+target.addEventListener("drop", drop);
 
-		function dragStart(event) {
-		  event.dataTransfer.setData("text/plain", event.target.id);
-		}
+function dragStart(event) {
+	event.dataTransfer.setData("text/plain", event.target.id);
+}
 
-		function dragOver(event) {
-		  event.preventDefault();
-		  target.classList.add("border-gray-500");
-		}
+function dragOver(event) {
+	event.preventDefault();
+	target.classList.add("border-gray-500");
+}
 
-		function drop(event) {
-		  event.preventDefault();
-		  const itemId = event.dataTransfer.getData("text/plain");
-		  const item = document.getElementById(itemId);
-		  target.appendChild(item);
-		  target.classList.remove("border-gray-500");
-		}
+function drop(event) {
+	event.preventDefault();
+	const itemId = event.dataTransfer.getData("text/plain");
+	const item = document.getElementById(itemId);
+	target.appendChild(item);
+	target.classList.remove("border-gray-500");
+}
